9aeb813e411d8022bb8dc136294881ce497c3f18
Licheng Zheng <licheng.111.zheng@gmail.com>
2026-01-10 13:29:48 -0500

    It did not work V2

--- Compoxel/src/model.py
+++ Compoxel/src/model.py
@@ -1,20 +1,27 @@
-import pyflamegpu
-from src.utils import load_kernel
-
 def create_model():
-    model = pyflamegpu.ModelDescription("MD_Sim")
+    model = pyflamegpu.ModelDescription("Compoxel_Sim")
     atom = model.newAgent("Atom")
     
-    # REQUIRED for Spatial3D
+    # 1. Setup Variables
     atom.newVariableFloat("x")
     atom.newVariableFloat("y")
     atom.newVariableFloat("z")
     
-    # Load our C++ kernel
+    # 2. REQUIRED: Create the Message
+    # If your C++ expects MessageSpatial3D, you MUST define it here
+    message = model.newMessageSpatial3D("location_message")
+    message.setMin(-100, -100, -100) # Must match your domain size
+    message.setMax(100, 100, 100)
+    message.setRadius(5.0)           # Interaction radius
+    
+    # 3. Setup the Function
     force_code = load_kernel("force.cu")
     force_func = atom.newRTCFunction("force_kernel", force_code)
     
-    # Just a placeholder layer for now
+    # 4. CRITICAL FIX: Connect the Message to the Function
+    force_func.setMessageInput("location_message") 
+    
+    # 5. Add to Layer
     model.newLayer().addAgentFunction(force_func)
     
     return model
